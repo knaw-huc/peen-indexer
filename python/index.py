613938e623a383039cc7a21749f1d988296bb9bf
HDJ <hayco.de.jong@di.huc.knaw.nl>
2025-02-03 11:49:59 +0100

    Iterate over all top tier annotations

--- python/index.py
+++ python/index.py
@@ -4,11 +4,10 @@ import logging
 import os
 import traceback
 from pathlib import Path
-from typing import List, Dict, Any
 
 import jsonpickle
 import requests
-from annorepo.client import AnnoRepoClient, ContainerAdapter
+from annorepo.client import AnnoRepoClient, ContainerAdapter, SearchInfo
 from elasticsearch import Elasticsearch
 from tqdm import tqdm
 
@@ -16,25 +15,40 @@ from SearchResultAdapter import SearchResultAdapter
 from SearchResultItem import SearchResultItem
 #from SparseList import SparseList
 
-def reset_index(elastic: Elasticsearch, index_name: str, path: str | os.PathLike):
-    mapping_path = Path(path)
+MAPPING_FILE='mapping.json'
 
+type Query = dict[str, str]
+
+def reset_index(elastic: Elasticsearch, index_name: str, path: str | os.PathLike) -> None:
     if elastic.indices.exists(index=index_name):
         print(f'Deleting ES index: {index_name}')
         elastic.indices.delete(index=index_name)
 
+    mapping_path = Path(path)
     print(f'Creating ES index: {index_name} using mapping from {mapping_path}')
     elastic.indices.create(index=index_name, body=mapping_path.read_text())
 
-def main(ar_host: str, ar_container: str, es_host: str, es_index):
+
+def index_suriano(container:ContainerAdapter, elastic:Elasticsearch, query: Query) -> None:
+    top_tier_anno_search: SearchResultAdapter = SearchResultAdapter(container, query)
+    count = 0
+    for anno in top_tier_anno_search.items():
+        print(f'annoId: {anno.path('body.id')}')
+        count += 1
+    print(f'Total of {count} top tier annos')
+
+
+def main(ar_host: str, ar_container: str, es_host: str, es_index) -> None:
     print(f'Indexing {ar_host}/{ar_container} to {es_host}/{es_index}')
     annorepo = AnnoRepoClient(ar_host)
     container = annorepo.container_adapter(ar_container)
-    print(annorepo.get_about())
+    print(annorepo.get_about(), container)
 
     elastic = Elasticsearch(es_host)
     print(elastic.info())
-    reset_index(elastic, es_index, 'mapping.json')
+    reset_index(elastic, es_index, MAPPING_FILE)
+
+    index_suriano(container, elastic, {"body.type": "LetterBody"})
 
 
 if __name__ == "__main__":
