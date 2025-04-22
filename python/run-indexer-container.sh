77810f207cf98967a7db9d485596a2428eaa0541
HDJ <hayco.de.jong@di.huc.knaw.nl>
2025-04-22 16:46:35 +0200

    Remove obsolete python/rust dirs

--- python/run-indexer-container.sh
+++
@@ -1,6 +0,0 @@
-docker run \
-    -e AR_HOST=https://annorepo.suriano.huygens.knaw.nl \
-    -e AR_CONTAINER=suriano-1.0.1e-029 \
-    -e ES_HOST=http://host.docker.internal:9200 \
-    -e ES_INDEX=surind-029 \
-    peen-indexer
